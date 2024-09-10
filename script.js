9bab81d1f1bfea4b2d103efa737266bfa0461c9c
francescofoglia0 <158574288+francescofoglia0@users.noreply.github.com>
2024-09-10 20:02:42 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     // Funzione per aggiornare la data e l'ora
     function aggiornaDataEOra() {
         const now = new Date();
-        
-        // Calcola l'orario 30 minuti indietro
-        const thirtyMinutesAgo = new Date(now.getTime() - 30 * 60 * 1000);
-        
         const optionsDate = { day: '2-digit', month: '2-digit', year: '2-digit' };
         const optionsTime = { hour: '2-digit', minute: '2-digit' };
 
+        // Calcola l'orario mezz'ora avanti
+        const futureTime = new Date(now.getTime() + 30 * 60000); // Aggiunge 30 minuti (30 * 60 * 1000 ms)
+
         const formattedDate = now.toLocaleDateString('it-IT', optionsDate);
-        const formattedTime = thirtyMinutesAgo.toLocaleTimeString('it-IT', optionsTime);
+        const formattedTime = futureTime.toLocaleTimeString('it-IT', optionsTime);
 
         // Aggiorna gli elementi della pagina
         document.querySelector('.data').textContent = formattedDate;
